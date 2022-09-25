a1794cf50307171fe7f1f06b0956dfcc066679ba
Jorge Ramirez <jarp9665@gmail.com>
2022-09-25 12:36:00 +0200

    Added Address and Birthdate
    Among other adjustments

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,8 @@ import "./Header.css";
 import photo from "../assets/img/photo.jpeg";
 
 const Header = ({ data }) => {
-  const { name, label, phone, email, profiles } = data;
+  const { name, label, phone, email, profiles, location } = data;
+  const { address, postalCode, city, region, country } = location;
   return (
     <div className="header">
       <div className="detail" />
@@ -37,6 +38,18 @@ const Header = ({ data }) => {
                 </a>
               );
             })}
+          </div>
+          <div className="header-center">
+            <div className="header-center">
+              <p className="icon-solid">crosshairs</p>
+              <h3>
+                {address}, {postalCode}, {city}, {region}, {country}
+              </h3>
+            </div>
+            <div className="header-center">
+              <p className="icon-solid">calendar-day</p>
+              <h3>28/05/1996</h3>
+            </div>
           </div>
         </div>
       </div>
